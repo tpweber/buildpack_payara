ed7c72b664c387952b399680e749c0158b9d2578
Thorsten Weber <thorsten.weber@jambit.com>
2016-01-25 12:57:48 +0100

    26th.

--- lib/java_buildpack/container/payara/payara_installer.rb
+++ lib/java_buildpack/container/payara/payara_installer.rb
@@ -86,7 +86,8 @@ module JavaBuildpack
           log("PayaraInstaller.install_using_zip: JAVA_BINARY: #{JAVA_BINARY}")
           log("PayaraInstaller.install_using_zip: File::FNM_DOTMATCH: #{File::FNM_DOTMATCH}")
 
-          log("PayaraInstaller.install_using_zip: #{Dir["."]}")
+          files = Dir.glob("#{@droplet.root}")
+          log("PayaraInstaller.install_using_zip: #{files}")
           oracle_jre_path = Dir.glob("**/oracle_jre/")[0]
           log("PayaraInstaller.install_using_zip: oracle_jre_path: #{oracle_jre_path}")
           java_binary = Dir.glob("#{@droplet.root}" + '/**/' + JAVA_BINARY)[0]
