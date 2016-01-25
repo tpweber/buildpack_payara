7cc36f262a2afdf3e7c83b5138cd946d907b625b
Thorsten Weber <thorsten.weber@jambit.com>
2016-01-25 14:27:25 +0100

    27th.

--- lib/java_buildpack/container/payara_as.rb
+++ lib/java_buildpack/container/payara_as.rb
@@ -60,7 +60,7 @@ module JavaBuildpack
 
           # Proceed with install under the APP-INF or WEB-INF folders
 
-          @payara_home = @droplet.root + '/' + PAYARA_ROOT_ELEMENT
+          @payara_home = @application.root + PAYARA_ROOT_ELEMENT
           log("Payara_AS.initialize: @payara_home -> #{@payara_home}")
 
           if app_inf?
